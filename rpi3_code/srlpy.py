a0b6728c17a66cdf3834edfadf1d8e55650e0c2b
AchiestDragon <achiestdragon@gmail.com>
2018-02-14 09:06:29 +0000

    code tidy of srlpy.py

--- rpi3_code/srlpy.py
+++ rpi3_code/srlpy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-
+#
 # ****************************************************************************
-# * srlpy.py test python script to read from all usb connected arduino nanos *
+# * srlpy.py    read lines concurrently from all usb connected arduino nanos *
+# ****************************************************************************
+# * version  1.0b                                              14th feb 2018 *
 # ****************************************************************************
 # *      (c)2018 & written by  David Powell  (A.K.A AchiestDragon)           *
 # ****************************************************************************
@@ -18,12 +20,13 @@
 #   /dev/ttyUSB2
 #   /dev/ttyUSB3
 #
-# and prints them to the console 
+# and prints there output to the console 
 #
 # notes :- 
 #
 #  if a serial port is not available that thread will exit with an error
 #  but the other threads will continue to run regardless 
+#
 #  ctrl+z to exit 
 #
 
@@ -31,69 +34,67 @@ import threading
 import time
 import serial
 
-
 def worker(num):
     if num == 0:
-        print 'Serial Read Worker: 1 port = /dev/ttyUSB0'
+        print 'Started Serial Read Worker:1 on port : /dev/ttyUSB0'
         ser0 = serial.Serial(
-         port='/dev/ttyUSB0',
-         baudrate = 9600,
-         parity=serial.PARITY_NONE,
-         stopbits=serial.STOPBITS_ONE,
-         bytesize=serial.EIGHTBITS,
-         timeout=1
+           port='/dev/ttyUSB0',
+           baudrate = 9600,
+           parity=serial.PARITY_NONE,
+           stopbits=serial.STOPBITS_ONE,
+           bytesize=serial.EIGHTBITS,
+           timeout=1
         )
         counter0=0
         while 1:
-          x0=ser0.readline()
-          print x0  
+            x0=ser0.readline()
+            print x0  
         return
     if num == 1:
-        print 'Serial Read Worker: 2 port = /dev/ttyUSB1'
+        print 'Started Serial Read Worker:2 on port : /dev/ttyUSB1'
         ser1 = serial.Serial(
-         port='/dev/ttyUSB1',
-         baudrate = 9600,
-         parity=serial.PARITY_NONE,
-         stopbits=serial.STOPBITS_ONE,
-         bytesize=serial.EIGHTBITS,
-         timeout=1
+           port='/dev/ttyUSB1',
+           baudrate = 9600,
+           parity=serial.PARITY_NONE,
+           stopbits=serial.STOPBITS_ONE,
+           bytesize=serial.EIGHTBITS,
+           timeout=1
         )
         counter1=0
         while 1:
-          x1=ser1.readline()
-          print x1  
+            x1=ser1.readline()
+            print x1  
         return
     if num == 2:
-        print 'Serial Read Worker: 3 port = /dev/ttyUSB2'
+        print 'Started Serial Read Worker:3 on port : /dev/ttyUSB2'
         ser2 = serial.Serial(
-         port='/dev/ttyUSB2',
-         baudrate = 9600,
-         parity=serial.PARITY_NONE,
-         stopbits=serial.STOPBITS_ONE,
-         bytesize=serial.EIGHTBITS,
-         timeout=1
+           port='/dev/ttyUSB2',
+           baudrate = 9600,
+           parity=serial.PARITY_NONE,
+           stopbits=serial.STOPBITS_ONE,
+           bytesize=serial.EIGHTBITS,
+           timeout=1
         )
         counter=0
         while 1:
-          x2=ser2.readline()
-          print x2  
+            x2=ser2.readline()
+            print x2  
         return
     if num == 3:
-        print 'Serial Read Worker: 4 port = /dev/ttyUSB3'
+        print 'Started Serial Read Worker:4 on port : /dev/ttyUSB3'
         ser3 = serial.Serial(
-         port='/dev/ttyUSB3',
-         baudrate = 9600,
-         parity=serial.PARITY_NONE,
-         stopbits=serial.STOPBITS_ONE,
-         bytesize=serial.EIGHTBITS,
-         timeout=1
+           port='/dev/ttyUSB3',
+           baudrate = 9600,
+           parity=serial.PARITY_NONE,
+           stopbits=serial.STOPBITS_ONE,
+           bytesize=serial.EIGHTBITS,
+           timeout=1
         )
         counter=0
         while 1:
-          x3=ser3.readline()
-          print x3  
+            x3=ser3.readline()
+            print x3  
         return    
- 
  
 threads = []
 for i in range(4):
