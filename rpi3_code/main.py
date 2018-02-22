bf054bfcab116e16cba07af2400e3855c40ade6c
AchiestDragon <achiestdragon@gmail.com>
2018-02-22 09:30:04 +0000

    test coe serial ack fix

--- rpi3_code/main.py
+++ rpi3_code/main.py
@@ -483,7 +483,7 @@ def walk_main_worker(srl_out_q,srl_in_q):
             
             # should handle ack responces better but for test here only
             cntr=0
-            while cntr != 3
+            while cntr != 3 :
                 if srl_in_q.startswith('k[') == true :
                     cntr = cntr +1
                     
